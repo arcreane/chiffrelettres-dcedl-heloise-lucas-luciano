707b299708bb77ef6c54f0339f29bcf98d1ea5ba
Lucas <lucas.gascoin@edu.esiee-it.fr>
2021-12-02 17:04:57 +0100

    same letter in dico to fix

--- src/com/company/lettres/dictionnaire.java
+++ src/com/company/lettres/dictionnaire.java
@@ -1,76 +1,187 @@
 package com.company.lettres;
 
-import java.util.ArrayList;
-import java.util.Objects;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Array;
+import java.util.*;
 
 public class dictionnaire {
-    private static final String[] consonne = {"b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","z"};
-    private static final String[] voyelle = {"a","e","i","o","u","y"};
-    static ArrayList<String> chosenLetters = new ArrayList<>();
+    static String[] arr;
+    private static final String[] consonne = {"b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"};
+    private static final String[] voyelle = {"a", "e", "i", "o", "u", "y"};
+    public static ArrayList<String> chosenLetters = new ArrayList<>();
     static int count = 0;
+    public static List<String> wordsFound = new ArrayList<>();
 
-    public static ArrayList<String> getLetters (String letter){
+    public static ArrayList<String> getLetters(String letter) {
 
-        if (letter.equals("voyelle") || letter.equals("v")){
+        if (letter.equals("voyelle") || letter.equals("v")) {
             chosenLetters.add(voyelle[rdnGen(voyelle.length)]);
+            System.out.println(chosenLetters);
             count++;
-        }
-        else if (letter.equals("consonne") || letter.equals("c")){
+        } else if (letter.equals("consonne") || letter.equals("c")) {
             chosenLetters.add(consonne[rdnGen(consonne.length)]);
+            System.out.println(chosenLetters);
             count++;
-        }
-        else if (count == 10){
+        } else if (count == 10) {
             return chosenLetters;
         }
         return null;
     }
 
-    private static int rdnGen(int z){
+    private static int rdnGen(int z) {
         int min = 0;
-        return (int) Math.floor(Math.random()*(z-min)+min);
+        return (int) Math.floor(Math.random() * (z - min) + min);
     }
 
-    private static boolean verifWord1 (String p1word){
-        boolean wordValidP1 = false;
-        ArrayList<String> cloneLetters = chosenLetters;
-        String [] tabP1 = p1word.split("");
+    public static String[] convertFileToArr() {
 
+        try {
+            List<String> lines = Files.readAllLines(Paths.get("/Users/lucasgascoin/Desktop/DCEDL/chiffrelettres-dcedl-heloise-lucas-luciano/src/liste_francais.txt"), StandardCharsets.ISO_8859_1);
+            arr = lines.toArray(new String[lines.size()]);
+            return arr;
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public static boolean verifWord1(String p1word) {
+        boolean wordValidP1 = true;
+        ArrayList<String> cloneLetters = new ArrayList<>();
         for (int i = 0; i < chosenLetters.size(); i++){
-            for (int j = 0; j < tabP1.length; j++){
-                if(Objects.equals(tabP1[j], chosenLetters.get(i))){
-                    wordValidP1 = true;
-                    cloneLetters.remove(i);
-                break;
+            cloneLetters.add(chosenLetters.get(i));
+        }
+
+        String[] tabP1 = p1word.split("");
+
+        for (int i = 0; i < tabP1.length; i++) {
+            if (wordValidP1) {
+                for (int j = 0; j < cloneLetters.size(); j++) {
+                    if (Objects.equals(tabP1[i], cloneLetters.get(j))) {
+                        wordValidP1 = true;
+                        cloneLetters.remove(j);
+                        break;
+                    } else {
+                        wordValidP1 = false;
+                    }
                 }
-            }
-            if (wordValidP1 == true){
-                return true;
-            }
-            else {
-                wordValidP1 = false;
+            } else {
+                return false;
             }
         }
+        List<String> listDico = Arrays.asList(arr);
+        if(listDico.contains(p1word)){
+            if (wordValidP1 == false) {
+                System.out.println("in if n-word false P1");
+
+                return false;
+            }
+            System.out.println("in if n-word true P1");
+
+            return true;
+        }
+        System.out.println("or if n-word false P1");
+
         return false;
     }
 
-    private static boolean verifWord2 (String p2word){
-        boolean wordValidP2 = false;
-        String [] tabP1 = p2word.split("");
-
+    public static boolean verifWord2(String p2word) {
+        boolean wordValidP2 = true;
+        ArrayList<String> cloneLetters = new ArrayList<>();
         for (int i = 0; i < chosenLetters.size(); i++){
-            for (int j = 0; j < tabP1.length; j++){
-                if(Objects.equals(tabP1[j], chosenLetters.get(i))){
-                    wordValidP2 = true;
+            cloneLetters.add(chosenLetters.get(i));
+        }
+        String[] tabP1 = p2word.split("");
+
+        for (int i = 0; i < tabP1.length; i++) {
+            if (wordValidP2) {
+                for (int j = 0; j < cloneLetters.size(); j++) {
+                    if (Objects.equals(tabP1[i], cloneLetters.get(j))) {
+                        wordValidP2 = true;
+                        cloneLetters.remove(j);
+                        break;
+                    } else {
+                        wordValidP2 = false;
+                    }
+                }
+            } else {
+                return false;
+            }
+        }
+        List<String> listDico = Arrays.asList(arr);
+        if(listDico.contains(p2word)){
+            if (wordValidP2 == false) {
+                System.out.println("in if n-word false P2");
+                return false;
+            }
+            System.out.println("in if n-word true P2");
+
+            return true;
+        }
+        System.out.println("or if n-word false P2");
+
+        return false;
+    }
+
+    public static void findWordInDico(String[] dico, List<String> letters){
+        String lettersCombined = String.join("", letters);
+
+
+        for (int i = 0; i < dico.length; i++) {
+            TreeMap<Character, Integer> freq = new TreeMap<Character, Integer>();
+
+            for (int j = 0; j < dico[i].length(); j++){
+                freq.put(dico[i].charAt(j), 0);
+            }
+
+            for (int j = 0; j < lettersCombined.length(); j++) {
+                if (freq.containsKey(lettersCombined.charAt(j)))
+                    freq.put(lettersCombined.charAt(j), freq.get(lettersCombined.charAt(j)) + 1);
+
+            }
+
+            boolean match = true;
+
+            for (int count : freq.values()) {
+                if (count <= 0) {
+                    match = false;
                     break;
                 }
-                else{
-                    wordValidP2 = false;
+            }
+            if (match)
+                wordsFound.add(dico[i]);
+                //System.out.println("'" + dico[i] + "' was found");
+        }
+    }
+    public static String longestWord(List<String> tss){
+        String wordRef = "";
+        ArrayList<String> cloneLetters = new ArrayList<>();
+
+
+        for (String mot : tss) {
+            cloneLetters.clear();
+            for (int i = 0; i < chosenLetters.size(); i++){
+                cloneLetters.add(chosenLetters.get(i));
+            }
+            String[] wordSplit = mot.split("");
+            for (int i = 0; i < mot.length(); i++){
+                System.out.println(cloneLetters);
+                for (int j = 0; j < cloneLetters.size(); j++) {
+                    System.out.println(cloneLetters);
+                    if (Objects.equals(wordSplit[i], cloneLetters.get(j))) {
+                        System.out.println(cloneLetters);
+                        cloneLetters.remove(i);
+                        if (mot.length() <= 10 && mot.length() > wordRef.length()) {
+                            wordRef = mot;
+                        }
+                    }
                 }
             }
-            if (wordValidP2 == true){
-                return true;
-            }
         }
-        return false;
+        return wordRef;
     }
 }
--- src/com/company/menu.java
+++ src/com/company/menu.java
@@ -2,10 +2,15 @@ package com.company;
 import com.company.chiffres.*;
 import com.company.lettres.*;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class menu {
+
+    static int scoreP1 = 0;
+    static int scoreP2 = 0;
+    static int scoreOrdi = 0;
 
     static void displayMenu() {
         System.out.println("""
@@ -17,14 +22,15 @@ public class menu {
 
     }
 
-     static void chooseOption() {
+     static void chooseOption() throws IOException {
         Scanner sc = new Scanner(System.in);
         displayMenu();
-        switch (sc.nextInt()) {
+         switch (sc.nextInt()) {
             case 1 -> secondChooseOption();
             case 2 -> System.out.println("ma bite");
             case 3 -> System.out.println("60% trop forte");
             case 4 -> System.exit(0);
+            //case 5 ->
         }
     }
 
@@ -55,7 +61,7 @@ public class menu {
                 String player1Name = sc.nextLine();
 
                 System.out.print("Nom joueur 2 : ");
-                String player2name = sc.nextLine();
+                String player2Name = sc.nextLine();
                 boolean turn = true;
                 for (int i = 0; i < 10; i++){
                     if (turn){
@@ -65,16 +71,48 @@ public class menu {
                         turn = false;
                     }
                     else{
-                        System.out.print("Joueur " + player2name + " quelle lettre voulez vous? ");
+                        System.out.print("Joueur " + player2Name + " quelle lettre voulez vous? ");
                         String answer = sc.nextLine();
                         dictionnaire.getLetters(answer);
                         turn = true;
                     }
                 }
+                dictionnaire.convertFileToArr();
                 System.out.print("Jouisseur " + player1Name + " insérez le mot que vous avez trouvé : ");
                 String answerP1 = sc.nextLine();
-                System.out.print("Jouisseur " + player1Name + " insérez le mot que vous avez trouvé : ");
+                System.out.print("Jouisseur " + player2Name + " insérez le mot que vous avez trouvé : ");
                 String answerP2 = sc.nextLine();
+
+                dictionnaire.findWordInDico(dictionnaire.convertFileToArr(), dictionnaire.chosenLetters);
+                if (dictionnaire.verifWord1(answerP1) && dictionnaire.verifWord2(answerP2)){
+                    if (answerP1.length() > answerP2.length()){
+                        scoreP1 += answerP1.length();
+                        System.out.println("Le joueur " + player1Name + " gagne la manche, ses points sont désormais de : " + scoreP1 + ".");
+
+                    }
+                    else if (answerP1.length() == answerP2.length()){
+                        System.out.println("Les mots ont la même taille, aucun joueur ne gagne donc de points.");
+                    }
+                    else {
+                        scoreP2 += answerP2.length();
+                        System.out.println("Le joueur " + player2Name + " gagne la manche, ses points sont désormais de : " + scoreP2 + ".");
+                    }
+                    System.out.println("Le mot le plus long trouvé dans le dictionnaire est : " + dictionnaire.longestWord(dictionnaire.wordsFound) + ".");
+                }
+                else if(dictionnaire.verifWord1(answerP1) && !dictionnaire.verifWord2(answerP2)){
+                    scoreP1 += answerP1.length();
+                    System.out.println("Le joueur " + player1Name + " gagne la manche, ses points sont désormais de : " + scoreP1 +".");
+                    System.out.println("Le mot le plus long trouvé dans le dictionnaire est : " + dictionnaire.longestWord(dictionnaire.wordsFound) + ".");
+                }
+                else if(!dictionnaire.verifWord1(answerP1) && dictionnaire.verifWord2(answerP2)){
+                    scoreP2 += answerP2.length();
+                    System.out.println("Le joueur " + player2Name + " gagne la manche, ses points sont désormais de : " + scoreP2 + ".");
+                    System.out.println("Le mot le plus long trouvé dans le dictionnaire est : " + dictionnaire.longestWord(dictionnaire.wordsFound) + ".");
+                }
+                else {
+                    System.out.println("Les deux mots ne sont pas valides, aucun des joueurs ne marque de points.");
+                    System.out.println("Le mot le plus long trouvé dans le dictionnaire est : " + dictionnaire.longestWord(dictionnaire.wordsFound) + ".");
+                }
             }
         }
     }
